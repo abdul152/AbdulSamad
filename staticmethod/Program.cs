90fb0ccd6571bda864736ae3143372820fd9838c
Abdul-Samad <Abdul-Samad@ABDUL-SAMAD>
2019-11-16 19:45:54 +0100

    commit

---
+++ staticmethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace staticmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = calc.add(5, 10);
+                Console.WriteLine(result);
+
+            int rus = calc.mul(5, 10);
+            Console.WriteLine(rus);
+
+
+        }
+    }
+}
---
+++ staticmethod/calc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace staticmethod
+{
+    class calc
+    {
+        public static int add(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 + Num2;
+
+            return Answer;
+                                 
+        }
+
+        public static int mul(int a, int b)
+        {
+            int Num3 = a;
+            int Num4 = b;
+            int Ans = Num3 * Num4;
+
+            return Ans;
+
+        }
+
+
+
+
+    }
+
+
+}
